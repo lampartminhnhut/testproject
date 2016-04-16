3f06b4598a3ba7f7d89c3918ac78f7e8f8e1f517
tmn17 <tmn17@DESKTOP-GGBF7ME>
2016-04-16 17:57:01 +0700

    asdas

--- modules/login/controllers/testController.php
+++ modules/login/controllers/testController.php
@@ -1,61 +1,110 @@
 <?php
 class indexController extends baseController {
-
-	public function index($arg = array()){
-
-		//session_start();
-		if( $this->checkLogin() ){
-			$this->redirect("user");
-			exit(0);
-		}
-		if(isset($_POST['login']) && $_POST['login']=='login'){
-			$user =  $_POST['username'];
-			$pass = $_POST['password'];
-			$model = $this->model->get('UserModel');
-			$account = $model->checkLogin($user,$pass);
-
-			if($account==false){
-				$error = 'Username and password wrong.';
-			}else{
-				$_SESSION['acl']['account'] = $account;
-				$this->redirectUrl( $this->url( array( 'module'=>'user' ) ) );
-				exit(0);
-			}
-		}
-
-		if( isset( $error ) ){
-			$this->getView()->content->error = $error;
-		}
-	}
-
-
-	public function logout(){
-
-		//session_start();
-		if( !$this->checkLogin() ){
-
-		}else if( $this->checkLogin() ){
-
-			/* @var $account User */
-			$_SESSION['acl']['account'] = new User();
-		}
-
-		$this->redirect("login");
-	}
-	
-	public function logout(){
-	
-	    //session_start();
-	    if( !$this->checkLogin() ){
-	
-	    }else if( $this->checkLogin() ){
-	
-	        /* @var $account User */
-	        $_SESSION['acl']['account'] = new User();
-	    }
-	
-	    $this->redirect("login");
-	}
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public function index($arg = array()) {
+        
+        // session_start();
+        if ($this->checkLogin ()) {
+            $this->redirect ( "user" );
+            exit ( 0 );
+        }
+        if (isset ( $_POST ['login'] ) && $_POST ['login'] == 'login') {
+            $user = $_POST ['username_newUserName'];
+            $pass = $_POST ['password_newPassword_branchMaster'];
+            $model = $this->model->get ( 'UserModel' );
+            $account = $model->checkLogin ( $user, $pass );
+            
+            if ($account == false) {
+                $error = 'Username and password wrong.';
+            } else {
+                $_SESSION ['acl'] ['account'] = $account;
+                $this->redirectUrl ( $this->url ( array (
+                        'module' => 'user' 
+                ) ) );
+                exit ( 0 );
+            }
+        }
+        
+        if (isset ( $error )) {
+            $this->getView ()->content->error = $error;
+        }
+    }
+    public function index($arg = array()) {
+        
+        // session_start();
+        if ($this->checkLogin ()) {
+            $this->redirect ( "user" );
+            exit ( 0 );
+        }
+        if (isset ( $_POST ['login'] ) && $_POST ['login'] == 'login') {
+            $user = $_POST ['username_newUserName'];
+            $pass = $_POST ['password_newPassword_branchMaster'];
+            $model = $this->model->get ( 'UserModel' );
+            $account = $model->checkLogin ( $user, $pass );
+            
+            if ($account == false) {
+                $error = 'Username and password wrong.';
+            } else {
+                $_SESSION ['acl'] ['account'] = $account;
+                $this->redirectUrl ( $this->url ( array (
+                        'module' => 'user' 
+                ) ) );
+                exit ( 0 );
+            }
+        }
+        
+        if (isset ( $error )) {
+            $this->getView ()->content->error = $error;
+        }
+    }
+    public function logout() {
+        
+        // session_start();
+        if (! $this->checkLogin ()) {
+        } else if ($this->checkLogin ()) {
+            
+            /* @var $account User */
+            $_SESSION ['acl'] ['account'] = new User ();
+        }
+        
+        $this->redirect ( "login" );
+    }
+    public function logout(){
+    
+        //session_start();
+        if( !$this->checkLogin() ){
+    
+        }else if( $this->checkLogin() ){
+    
+            /* @var $account User */
+            $_SESSION['acl']['account'] = new User();
+        }
+    
+        $this->redirect("login");
+    }
+    
+    public function logout(){
+    
+        //session_start();
+        if( !$this->checkLogin() ){
+    
+        }else if( $this->checkLogin() ){
+    
+            /* @var $account User */
+            $_SESSION['acl']['account'] = new User();
+        }
+    
+        $this->redirect("login");
+    }
 }
 ?>
